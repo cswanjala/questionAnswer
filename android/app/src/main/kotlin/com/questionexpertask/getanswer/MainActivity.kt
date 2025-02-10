2c2fd40fe3260a26ae8ddf1edff027855fd7507f
cswanjala <cswanjala254@gmail.com>
2025-02-10 20:12:25 +0300

    final changes

--- android/app/src/main/kotlin/com/questionexpertask/getanswer/MainActivity.kt
+++ android/app/src/main/kotlin/com/questionexpertask/getanswer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.expertask.answer
+package com.questionexpertask.getanswer
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
